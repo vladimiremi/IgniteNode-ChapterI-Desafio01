263bfe46e0cedd7f82e9dbd9c89c5aa0562ade44
vladimiremi <vladimiremi2014@gmail.com>
2023-01-29 22:05:50 -0300

    feat: add post todos endpoint

--- src/index.js
+++ src/index.js
@@ -47,7 +47,28 @@ app.get("/todos", checksExistsUserAccount, (request, response) => {
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { username } = request.headers;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  const addTodo = users.map((user) => {
+    if (user.username === username) {
+      user.todos = [...user.todos, todo];
+    }
+
+    return user;
+  });
+
+  users = addTodo;
+
+  return response.status(201).json(todo);
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
